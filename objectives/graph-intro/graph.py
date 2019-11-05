cb8d2c26134cbecb1fedca5443429ae0330c5750
abidex4yemi <abidex4yemi@gmail.com>
2019-11-05 08:22:12 +0100

    Implement stack ds

--- objectives/graph-intro/graph.py
+++ objectives/graph-intro/graph.py
@@ -4,10 +4,22 @@ class Graph:
 
     def add_vertex(self, vertex_id):
         if vertex_id is not None:
-            self.vertices[vertex_id] = self.vertices[vertex_id] or set()
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
         else:
             raise KeyError("Vertex does not exist")
+
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+# graph.add_edge('0', '4')
+print(graph.vertices)
